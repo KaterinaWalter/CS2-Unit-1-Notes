4a6061cf7f790d9e3f049b8d42fbde5809311c34
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2025-09-12 12:56:34 +0000

    1.2 notes

--- Main.java
+++ Main.java
@@ -1,8 +1,23 @@
 // Every Java file is a CLASS
 public class Main {
-   // main method is the ENTRYWAY to the program
+   // main METHOD is the ENTRYWAY to run the program
    public static void main(String []args) {
       System.out.println("Welcome to Java!");
       System.out.println("hiiii");
+
+      // 1. DECLARE a new variable
+      int score; 
+      // 2. ASSIGN a value to the variable
+      score = 72; 
+      // INITIALIZE combines declaring + assigning
+      String pokemon = "Pikachu";
+      double healthPercent = 0.75;
+      boolean isAlive = true; 
+
+      // Use the plus sign to CONCATENATE (combine) Strings
+      String greeting = "Hello "           + "World";
+      System.out.println(greeting);
+      // Quotation marks contain STRING LITERALS
+
    }
 }
