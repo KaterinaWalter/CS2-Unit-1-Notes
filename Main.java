5fc573a91a41fd009f69fe4afe4334effc4c9247
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2025-09-30 14:13:57 +0000

    1.6 notes

--- Main.java
+++ Main.java
@@ -106,5 +106,23 @@ public class Main {
       int nearestPositive = (int) (positiveNum + 0.5);
       int nearestNegative = (int) (negativeNum - 0.5); 
 
+      // COMPOUND OPERATORS (shortcuts)
+      int runs = 0; 
+      // Re-assign variable to existing value plus 1
+      runs = runs + 1; 
+      runs += 1; // operator first, then result assigned back
+      System.out.println("Runs: " + runs);
+      // Even shorter shortcut for adding/subtracting ONE!
+      runs++;
+      runs--; // same as runs = runs - 1;
+
+      // More examples of shortcuts
+      runs = runs * 2; // runs is now 4
+      runs *= 3; // runs is now 12
+      System.out.println("Runs is now " + runs);
+      runs *= -10; 
+      runs /= 10; // dividing current value by 10
+      runs %= 7; // remainder of runs/7
+      
    }
 }
